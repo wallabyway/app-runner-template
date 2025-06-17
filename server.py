7c683c4569739c571c9fc081eb4194970eb0ba51
Michael Beale <bealester@gmail.com>
2025-06-17 15:11:28 -0700

    bind to all ports for AWS, turn on prod mode

--- server.py
+++ server.py
@@ -23,4 +23,4 @@ def api_list():
     token = get_token()
     return jsonify(list_bucket(token))
 if __name__ == '__main__':
-    app.run(port=8080) 
+    app.run(host='0.0.0.0', port=8080, debug=False)
